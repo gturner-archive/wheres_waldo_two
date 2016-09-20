9560e302bfb5fa2285a5e32661abb708c760d2f4
Adrian Mui <adrian.mui@gmail.com>
2016-09-20 11:06:39 -0500

    tags

--- find_waldo/app/assets/javascripts/finds.js
+++ find_waldo/app/assets/javascripts/finds.js
@@ -1,27 +1,60 @@
 $(document).ready(function() {
-  WALDO.taggerView.clickListener();
+  WALDO.taggerController.init();
 });
 
 var WALDO = WALDO || {};
 
 WALDO.tagger = {
+  init: function() {
+
+  },
+
+  createTempBox: function(x, y) {
+    WALDO.tagger.tempBoxCoords = [x,y];
+  },
+
+
 
 };
 
 WALDO.taggerView = {
+  xOffset: 50,
+  yOffset: 50,
+
+  init: function() {
+
+    WALDO.taggerView.clickListener();
+  },
 
   clickListener: function() {
-    $('.waldo-img').on('click', pictureClick)
+    $('.waldo-img').on('click', WALDO.taggerView.pictureClick);
   },
 
   pictureClick: function(e) {
-    var x = e.offsetX;
-    var y = e.offsetY;
-    WALDO.
+    var x = e.offsetX - WALDO.taggerView.xOffset;
+    var y = e.offsetY - WALDO.taggerView.yOffset;
+ 
+    WALDO.taggerController.createTempTag(x,y);
+  },
+
+  render: function(coordsArr) {
+    $box = $('<div>')
+      .addClass('temp-tag')
+      .css("top", coordsArr[1])
+      .css("left", coordsArr[0]);
+    $('body').append($box);
   }
 
 }
 
 WALDO.taggerController = {
+  init : function() {
+    WALDO.taggerView.init();
+  },
 
+  createTempTag: function(x, y) {
+    WALDO.tagger.createTempBox(x, y);
+    WALDO.taggerView.render(WALDO.tagger.tempBoxCoords);
+  }
 };
+
