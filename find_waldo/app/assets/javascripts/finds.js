2f11f02130481bf0918eacdbb4fc121e34b8721d
Adrian Mui <adrian.mui@gmail.com>
2016-09-20 11:58:25 -0500

    okok

--- find_waldo/app/assets/javascripts/finds.js
+++ find_waldo/app/assets/javascripts/finds.js
@@ -8,12 +8,24 @@ WALDO.tagger = {
 
   names: ['Graham', 'Adrian'],
 
+  permanent: [],
+
   init: function() {
 
   },
 
   createTempBox: function(x, y) {
     WALDO.tagger.tempBoxCoords = [x,y];
+  },
+
+  createBox: function(name) {
+    var person = {
+      name: name,
+      x: this.tempBoxCoords[0],
+      y: this.tempBoxCoords[1]
+    }
+    this.permanent.push(person);
+    this.tempBoxCoords = [];
   },
 
 
@@ -40,28 +52,36 @@ WALDO.taggerView = {
     WALDO.taggerController.createTempTag(x,y);
   },
 
-  nameClickListener = function(e) {
-    $('.name-item').on('click', WALDO.taggerView.nameClick);
+  nameClickListener: function() {
+    $('body').on('click', '.name-item', WALDO.taggerView.nameClick);
   },
 
-  nameClick: function() {
-
+  nameClick: function(e) {
+    var name = $(e.target).text();
+    WALDO.taggerController.createTag(name);
   },
 
 
   //REFACTOR
-  render: function(coordsArr, names) {
+  render: function(coordsArr, namesArr, permArr) {
     $('.temp-tag').remove();
     $('.dropdown').remove();
 
+    if (coordsArr[0]) {
+      this.renderTemp(coordsArr, namesArr);
+    }
+    if (permArr[0]) {
+      this.renderPermanent(permArr);  
+    }
+    
+  },
+
+  renderTemp: function(coordsArr, namesArr) {
     var $dropdown = $('<div>')
     var $namesBox = $('<ul>').addClass('name-list')
 
-    for (var i in names) {
-      var $li = $('<li>')
-      $li.text(names[i]);
-      $li.addClass('name-item')
-      $namesBox.append($li)
+    for (var i in namesArr) {
+      this.buildListItems(namesArr[i], $namesBox);
     }
 
     $dropdown.append($namesBox);
@@ -70,15 +90,39 @@ WALDO.taggerView = {
       .css("top", coordsArr[1] + WALDO.taggerView.yOffset * 2)
       .css("left", coordsArr[0]);
 
-    $box = $('<div>')
+    var $box = $('<div>')
       .addClass('temp-tag')
       .css("top", coordsArr[1])
       .css("left", coordsArr[0]);
     $('body').append($box);
     $('body').append($dropdown);
+  },
+
+  buildListItems: function(name, $namesBox) {
+    var $li = $('<li>');
+    $li.text(name);
+    $li.addClass('name-item');
+    $namesBox.append($li);
+  },
+
+  renderPermanent: function(permArr) {
+    for (var i in permArr) {
+      var person = permArr[i];
+      var $box = $('<div>')
+        .addClass('perm-tag')
+        .css("top", person.y)
+        .css("left", person.x);
+      var $nameBox = $('<div>')
+        .addClass('name-box')
+        .text(person.name)
+        .css("top", person.y + WALDO.taggerView.yOffset * 2)
+        .css("left", person.x)
+      $('body').append($box);
+      $('body').append($nameBox);
+    }
   }
 
-}
+};
 
 WALDO.taggerController = {
   init : function() {
@@ -87,6 +131,11 @@ WALDO.taggerController = {
 
   createTempTag: function(x, y) {
     WALDO.tagger.createTempBox(x, y);
-    WALDO.taggerView.render(WALDO.tagger.tempBoxCoords, WALDO.tagger.names);
-  }
+    WALDO.taggerView.render(WALDO.tagger.tempBoxCoords, WALDO.tagger.names, WALDO.tagger.permanent);
+  },
+
+  createTag: function(name) {
+    WALDO.tagger.createBox(name);
+    WALDO.taggerView.render(WALDO.tagger.tempBoxCoords, WALDO.tagger.names, WALDO.tagger.permanent);
+  },
 };
